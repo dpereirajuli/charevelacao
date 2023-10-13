c9a8111d71c40ca8c4bf889ac281e2b98d7aec65
Daniel Pereira <131875304+dpereirajuli@users.noreply.github.com>
2023-10-13 19:42:12 -0300

    Add files via upload

--- processa.php
+++ processa.php
@@ -6,7 +6,7 @@ if(isset($_POST['button-processa'])){
     $apertado = $_POST['button-processa'];
     $sql = "UPDATE `tb_enquete` SET `$apertado`= (SELECT $apertado tb_enquete) +1";
     $result = mysqli_query($conexao,$sql);
-    header('Location: index.php?voto');
+    header('Location: index.html?voto');
     
 }
 else{
